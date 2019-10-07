5cfda9e82e0005f2a0fab65a4d431cade2ad03ec
Maciej Kruszewski <m.kruszewski@sunflux.pl>
2019-10-08 00:22:51 +0200

    Just display

--- react-frontend/src/components/MyComponent.jsx
+++ react-frontend/src/components/MyComponent.jsx
@@ -6,14 +6,62 @@ import React, {useEffect, useState} from "react"
  * @constructor
  */
 const MyComponent = () => {
-    const [name, setName] = useState("Luke");
+    const [person, setPerson] = useState({
+        name: '',
+        birthYear: '',
+        eyeColor: '',
+        gender: '',
+        hairColor: '',
+        mass: '',
+        height: '',
+        homeworld: '',
+        skinColor: '',
+        created: '',
+    });
+    const axios = require('axios');
+    const getRandomPerson = async () => {
+        let requestPerson = {};
+
+        await axios.get('http://localhost:8080/api/v1/sw/characters/random')
+            .then(function (response) {
+                requestPerson = response.data;
+                console.log(response);
+                console.log('requestPerson', requestPerson);
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+
+                return {}
+            });
+
+        return requestPerson;
+    };
+
     useEffect(() => {
         //Call spring back-end
-        setName("Luke")
+        getRandomPerson().then((v) => {
+            console.log('v', v)
+            setPerson(v)
+        })
     }, []);
 
     return <div>
-        Name: {name}
+        <div>
+            Name: {person.name || ''} <br/>
+            Birth Year: {person.birthYear || ''} <br/>
+            Eye Color: {person.eyeColor || ''} <br/>
+            Gender: {person.gender || ''} <br/>
+            Hair Color: {person.hairColor || ''} <br/>
+            Height: {person.height || ''} <br/>
+            Mass: {person.mass || ''} <br/>
+            Skin Color: {person.skinColor || ''} <br/>
+            Homeworld: {person.homeworld || ''} <br/>
+            Created: {person.created || ''} <br/>
+        </div>
+
+        <button onClick={() => getRandomPerson().then((v) => setPerson(v))}>I want yoda!</button>
     </div>
 };
 
